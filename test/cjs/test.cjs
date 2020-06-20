462375e04a223851fcb41e9731725f94c18747d3
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-06-20 03:19:00 +0000

    fix: test improvements

--- test/cjs/test.cjs
+++ test/cjs/test.cjs
@@ -1,2 +1,11 @@
+/* globals it */
+const assert = require('assert')
 const multiformats = require('multiformats')
-console.log(multiformats)
+
+const same = assert.deepStrictEqual
+const test = it
+
+test('multiformat imports basics', () => {
+  same(typeof multiformats.encode, 'function')
+  same(typeof multiformats.decode, 'function')
+})
