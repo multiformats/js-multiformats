a44c49dfc1f59b575ed34a656f1a5332830d3ebf
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-06-20 06:04:41 +0000

    fix: export fixes for cjs/import

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,23 @@
-export default {
-  input: 'basics.js',
-  output: {
-    file: 'dist/bundle.cjs',
-    format: 'cjs'
-  }
+import globby from 'globby'
+import path from 'path'
+
+let configs = []
+
+const _filter = p => !p.includes('/_') && !p.includes('rollup.config')
+
+const add = pattern => {
+  configs = configs.concat(globby.sync(pattern).filter(_filter).map(inputFile => ({
+    input: inputFile,
+    output: {
+      file: path.join('dist', inputFile).replace('.js', '.cjs'),
+      format: 'cjs'
+    }
+  })))
 }
+add('*.js')
+add('bases/*.js')
+add('hashes/*.js')
+add('codecs/*.js')
+console.log({ configs })
+
+export default configs
--- test/cjs/test.cjs
+++ test/cjs/test.cjs
@@ -1,11 +1,23 @@
 /* globals it */
-const assert = require('assert')
-const multiformats = require('multiformats')
+const modules = {}
+modules.multiformats = require('multiformats')
+modules['multiformats/basics.js'] = require('multiformats/basics.js')
+modules['multiformats/bytes.js'] = require('multiformats/bytes.js')
+modules['multiformats/cid.js'] = require('multiformats/cid.js')
+modules['multiformats/legacy.js'] = require('multiformats/legacy.js')
+modules['multiformats/bases/base16.js'] = require('multiformats/bases/base16.js')
+modules['multiformats/bases/base32.js'] = require('multiformats/bases/base32.js')
+modules['multiformats/bases/base58.js'] = require('multiformats/bases/base58.js')
+modules['multiformats/bases/base64.js'] = require('multiformats/bases/base64.js')
+modules['multiformats/hashes/sha2-browser.js'] = require('multiformats/hashes/sha2-browser.js')
+modules['multiformats/hashes/sha2.js'] = require('multiformats/hashes/sha2.js')
+modules['multiformats/codecs/json.js'] = require('multiformats/codecs/json.js')
+modules['multiformats/codecs/raw.js'] = require('multiformats/codecs/raw.js')
 
-const same = assert.deepStrictEqual
 const test = it
 
 test('multiformat imports basics', () => {
-  same(typeof multiformats.encode, 'function')
-  same(typeof multiformats.decode, 'function')
+  for (const [, value] of Object.entries(modules)) {
+    if (typeof value === 'undefined') throw new Error('Missing export')
+  }
 })
