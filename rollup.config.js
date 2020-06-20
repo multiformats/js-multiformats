ff92023c92033e87aa64b7778b551e1e3e9ce8f2
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-06-20 16:44:34 +0000

    test: compile all test to cjs and run

--- rollup.config.js
+++ rollup.config.js
@@ -5,10 +5,25 @@ let configs = []
 
 const _filter = p => !p.includes('/_') && !p.includes('rollup.config')
 
-const add = pattern => {
+const relativeToMain = name => ({
+  name: 'relative-to-main',
+  renderChunk: source => {
+    while (source.includes("require('../index.js')")) {
+      source = source.replace("require('../index.js')", "require('multiformats')")
+    }
+    while (source.includes("require('../")) {
+      source = source.replace('require(\'../', 'require(\'multiformats/')
+    }
+    return source
+  }
+})
+
+const plugins = [relativeToMain('multiformats')]
+const add = (pattern) => {
   configs = configs.concat(globby.sync(pattern).filter(_filter).map(inputFile => ({
     input: inputFile,
     output: {
+      plugins: pattern.startsWith('test') ? plugins : null,
       file: path.join('dist', inputFile).replace('.js', '.cjs'),
       format: 'cjs'
     }
@@ -18,6 +33,8 @@ add('*.js')
 add('bases/*.js')
 add('hashes/*.js')
 add('codecs/*.js')
-console.log({ configs })
+add('test/*.js')
+add('test/fixtures/*.js')
+console.log(configs)
 
 export default configs
