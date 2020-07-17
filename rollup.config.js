3a578d4db659b4e9971514dfd587972b0ba6f52b
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-07-17 19:01:21 +0000

    build: fix cjs builds
    something we didn't realize when moving to named imports is that rollup doesn't consider those imports modules to be preserved, so we can't rely on the tests to find all the required files to build anymore

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,14 @@ import { join } from 'path'
 
 const dir = 'dist'
 const preserveModules = true
-const output = { dir, preserveModules, format: 'cjs', entryFileNames: '[name].cjs' }
-const testdir = join(__dirname, 'test')
-const filter = name => name.startsWith('test-')
-const createConfig = f => ({ input: join('test', f), output })
-
-const configs = readdirSync(testdir).filter(filter).map(createConfig)
-
+const configs = []
+for (const subdir of ['test', 'bases', 'hashes', 'codecs', '.']) {
+  const output = { dir: join(dir, subdir), preserveModules, format: 'cjs', entryFileNames: '[name].cjs' }
+  const testdir = join(__dirname, subdir)
+  const filter = name => name.endsWith('.js')
+  const createConfig = f => ({ input: join(subdir, f), output })
+  readdirSync(testdir).filter(filter).map(createConfig).forEach(c => configs.push(c))
+}
 console.log(configs)
 
 export default configs
