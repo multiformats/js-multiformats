222ad1c93275102eacb9adb00dee4687b744b854
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-07-17 19:15:14 +0000

    fix: back to full dynamic rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,17 @@
-import { readdirSync } from 'fs'
-import { join } from 'path'
-import alias from '@rollup/plugin-alias';
+import { readdirSync, readFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+import alias from '@rollup/plugin-alias'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const pkg = JSON.parse(readFileSync(join(__dirname, 'package.json')))
 
 const dir = 'dist'
 const preserveModules = true
 const plugins = [
-  alias({ entries: [ { find: 'multiformats', replacement: process.cwd() } ] })
+  alias({ entries: [{ find: pkg.name, replacement: __dirname }] })
 ]
 
 const output = { dir, preserveModules, format: 'cjs', entryFileNames: '[name].cjs' }
