e100a54b7aa74c46399eb2630567b5d5b71553c3
Mikeal Rogers <mikeal.rogers@gmail.com>
2020-07-17 19:11:53 +0000

    fix: back to fully automated build using test import tree

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,19 @@
 import { readdirSync } from 'fs'
 import { join } from 'path'
+import alias from '@rollup/plugin-alias';
 
 const dir = 'dist'
 const preserveModules = true
-const configs = []
-for (const subdir of ['test', 'bases', 'hashes', 'codecs', '.']) {
-  const output = { dir: join(dir, subdir), preserveModules, format: 'cjs', entryFileNames: '[name].cjs' }
-  const testdir = join(__dirname, subdir)
-  const filter = name => name.endsWith('.js')
-  const createConfig = f => ({ input: join(subdir, f), output })
-  readdirSync(testdir).filter(filter).map(createConfig).forEach(c => configs.push(c))
-}
+const plugins = [
+  alias({ entries: [ { find: 'multiformats', replacement: process.cwd() } ] })
+]
+
+const output = { dir, preserveModules, format: 'cjs', entryFileNames: '[name].cjs' }
+const testdir = join(__dirname, 'test')
+const filter = name => name.endsWith('.js')
+const createConfig = f => ({ input: join('test', f), output, plugins })
+const configs = readdirSync(testdir).filter(filter).map(createConfig)
+
 console.log(configs)
 
 export default configs
