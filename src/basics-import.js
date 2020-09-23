7d2fda316ab81dddf68add4445f5da62d9c50ab6
Irakli Gozalishvili <contact@gozala.io>
2020-09-23 00:49:20 -0700

    chore: remove block.js

--- src/basics-import.js
+++ src/basics-import.js
@@ -1,6 +1,6 @@
 
-import { CID, Block, hasher, digest, varint, bytes, hashes, codecs, bases as _bases } from './basics.js'
+import { CID, hasher, digest, varint, bytes, hashes, codecs, bases as _bases } from './basics.js'
 import * as base64 from './bases/base64-import.js'
 
 const bases = { ..._bases, ...base64 }
-export { CID, Block, hasher, digest, varint, bytes, hashes, codecs, bases }
+export { CID, hasher, digest, varint, bytes, hashes, codecs, bases }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 // @ts-check
 
 import CID from './cid.js'
-import Block from './block.js'
 import * as varint from './varint.js'
 import * as bytes from './bytes.js'
 import * as hasher from './hashes/hasher.js'
 import * as digest from './hashes/digest.js'
 import * as codec from './codecs/codec.js'
 
-export { CID, Block, hasher, digest, varint, bytes, codec }
+export { CID, hasher, digest, varint, bytes, codec }
