cea9063c7c2b3f76bfd7b69efd7ca5c54eb613be
Volker Mische <volker.mische@gmail.com>
2021-03-26 12:21:19 +0100

    feat!: named exports instead of default exports
    It also removes the `Codec` class and exports a plain object
    instead. This has consequences for imports.
    
    BREAKING CHANGE:
    
    Only use names exports as default exports don't play well with
    CommonJS + Typescript typing.
    
    This means that when you use ESM imports, e.g. the raw codec is
    no longer imported as
    
    ```js
    import raw from 'multiformats/codecs/raw'
    ```
    
    but as
    
    ```js
    import * as raw from 'multiformats/codecs/raw'
    ```
    
    The CJS import for codecs don't change, it's still `const raw = require('multiformats/codecs/raw`.
    
    Though other imports change, so
    
    ```js
    import CID from 'multiformats/cid'
    const CID = require('multiformats/cid')
    ```
    
    is now
    
    ```js
    import { CID } from 'multiformats/cid'
    const { CID } = require ('multiformats/cid')
    ```

--- src/bytes.js
+++ src/bytes.js
@@ -33,7 +33,8 @@ const equals = (aa, bb) => {
 }
 
 /**
- * @param {ArrayBufferView|ArrayBuffer} o
+ * @param {ArrayBufferView|ArrayBuffer|Uint8Array} o
+ * @returns {Uint8Array}
  */
 const coerce = o => {
   if (o instanceof Uint8Array && o.constructor.name === 'Uint8Array') return o
