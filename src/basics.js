8fa28ac0542746a4823410c156f94f38480d3490
achingbrain <alex@achingbrain.net>
2021-07-12 13:20:55 +0100

    chore: add missing basics to basics export

--- src/basics.js
+++ src/basics.js
@@ -1,16 +1,20 @@
 // @ts-check
 
+import * as base16 from './bases/base16.js'
 import * as base32 from './bases/base32.js'
+import * as base36 from './bases/base36.js'
 import * as base58 from './bases/base58.js'
+import * as base64 from './bases/base64.js'
 import * as sha2 from './hashes/sha2.js'
+import * as identity from './hashes/identity.js'
 
 import * as raw from './codecs/raw.js'
 import * as json from './codecs/json.js'
 
 import { CID, hasher, digest, varint, bytes } from './index.js'
 
-const bases = { ...base32, ...base58 }
-const hashes = { ...sha2 }
+const bases = { ...base16, ...base32, ...base36, ...base58, ...base64 }
+const hashes = { ...sha2, ...identity }
 const codecs = { raw, json }
 
 export { CID, hasher, digest, varint, bytes, hashes, bases, codecs }
