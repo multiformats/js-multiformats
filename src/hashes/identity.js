e58fc416482f74721aae06fcd0e75d70e94fbf85
Irakli Gozalishvili <contact@gozala.io>
2022-01-20 09:41:40 -0800

    fix: coverage by using encode

--- src/hashes/identity.js
+++ src/hashes/identity.js
@@ -4,16 +4,13 @@ import * as Digest from './digest.js'
 const code = 0x0
 const name = 'identity'
 
-/**
- * @param {Uint8Array} input
- * @returns {Uint8Array}
- */
-const encode = (input) => coerce(input)
+/** @type {(input:Uint8Array) => Uint8Array} */
+const encode = coerce
 
 /**
  * @param {Uint8Array} input
  * @returns {Digest.Digest<typeof code, number>}
  */
-const digest = (input) => Digest.create(code, coerce(input))
+const digest = (input) => Digest.create(code, encode(input))
 
 export const identity = { code, name, encode, digest }
