357347ccffac67add02ce845a554968c166bee44
Rod Vagg <rod@vagg.org>
2022-01-20 11:29:52 +1100

    fix: export only `identity` hasher const
    basics.js uses `...identity` from the import, so `name` and friends
    get mixed up as hashers.
    
    Instead of removing `identity` and making basics import a single multihasher
    from `*`, we'll fix this for now (non-breaking) and maybe introduce a breaking
    change to switch the export later.
    
    Fixes: https://github.com/ipfs/js-ipfs/issues/4020

--- src/hashes/identity.js
+++ src/hashes/identity.js
@@ -1,14 +1,14 @@
 import { coerce } from '../bytes.js'
 import * as Digest from './digest.js'
 
-export const code = 0x0
-export const name = 'identity'
+const code = 0x0
+const name = 'identity'
 
 /**
  * @param {Uint8Array} input
  * @returns {Digest.Digest<typeof code, number>}
  */
-export const digest = (input) => Digest.create(code, coerce(input))
+const digest = (input) => Digest.create(code, coerce(input))
 
 /** @type {import('./interface').SyncMultihashHasher<typeof code>} */
 export const identity = { code, name, digest }
