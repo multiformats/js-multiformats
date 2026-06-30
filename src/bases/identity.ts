cb4eb32aa6d86ba4558146550f340f0191b21c76
Alex Potsides <alex@achingbrain.net>
2026-06-30 12:39:59 +0200

    fix: constrain identity bytes to single characters (#338)
    Use the char code API to interpret bytes as single characters to instead of using lossy utf-8 conversion.
    
    Fixes: #122

--- src/bases/identity.ts
+++ src/bases/identity.ts
@@ -1,9 +1,9 @@
-import { fromString, toString } from '../bytes.ts'
+import { coerce, fromString, toString } from '../bytes.ts'
 import { from } from './base.ts'
 
 export const identity = from({
   prefix: '\x00',
   name: 'identity',
-  encode: (buf) => toString(buf),
-  decode: (str) => fromString(str)
+  encode: (buf) => toString(coerce(buf)),
+  decode: (str) => coerce(fromString(str))
 })
